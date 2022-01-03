a4bf62f56c1fd36a5db61736d37d83632a3c81cc
joao <joaoluisazevedoferreira@gmail.com>
2022-01-03 21:01:30 +0000

    Done winrates and popularity by elo

--- plot-popularity.js
+++ plot-popularity.js
@@ -1,7 +1,14 @@
+var div=document.querySelector('#plot-popularity');
+
+
+var popularity1 = div.getAttribute("pop1");
+var popularity2 = div.getAttribute("pop2");
+var popularity3 = div.getAttribute("pop3");
+
 var data = [
     {
-      x: ['1000', '1500', '2000'],
-      y: [2, 5, 1],
+      x: ['<1200', '1200-1800', '>1800'],
+      y: [popularity1, popularity2, popularity3],
       type: 'bar'
     }
   ];
--- plot-rates.js
+++ plot-rates.js
@@ -1,29 +1,38 @@
 
 var div=document.querySelector('#plot-rates');
-var white_games=div.getAttribute("white");
-var draw_games=div.getAttribute("draw");
-var black_games=div.getAttribute("black");
-var games=div.getAttribute("games");
+
+
+var white1 = div.getAttribute("white1");
+var white2 = div.getAttribute("white2");
+var white3 = div.getAttribute("white3");
+
+var black1 = div.getAttribute("black1");
+var black2 = div.getAttribute("black2");
+var black3 = div.getAttribute("black3");
+
+var draw1 = div.getAttribute("draw1");
+var draw2 = div.getAttribute("draw2");
+var draw3 = div.getAttribute("draw3");
 
 var white_win = {
-    x: ['1000'],
-    y: [(white_games/games)*100],
+    x: ['<1200','1200-1800','>1800'],
+    y: [white1*100, white2*100, white3*100],
     name: 'White win rate',
     marker: {color: 'rgb(220,220,220)'},
     type: 'bar'
   };
   
   var draw = {
-    x: ['1000'],
-    y: [(draw_games/games)*100],
+    x: ['<1200','1200-1800','>1800'],
+    y: [draw1*100, draw2*100, draw3*100],
     name: 'Draw rate',
     marker: {color: 'rgb(150,150,150)'},
     type: 'bar'
   };
 
   var black_win = {
-    x: ['1000'],
-    y: [(black_games/games)*100],
+    x: ['<1200','1200-1800','>1800'],
+    y: [black1*100, black2*100, black3*100],
     name: 'Black win rate',
     marker: {color: 'rgb(0,0,0)'},
     type: 'bar'
